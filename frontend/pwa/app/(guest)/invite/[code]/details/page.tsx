a131d5a8a82451a97d831dcbfcf8d8518e42d2ca
Manuel Ruíz <42383773+ManuelRuiz27@users.noreply.github.com>
2025-10-15 18:42:17 -0600

    Update guest invite confirmation flow

--- frontend/pwa/app/(guest)/invite/[code]/details/page.tsx
+++ frontend/pwa/app/(guest)/invite/[code]/details/page.tsx
@@ -19,6 +19,7 @@ export default async function InviteDetailsPage({ params }: PageProps) {
   return (
     <div style={{ ...surfaceBase, ...themeToStyle(invite.theme) }}>
       <EventInfo
+        code={invite.code}
         event={invite.event}
         guest={invite.guest}
         template={invite.template}
